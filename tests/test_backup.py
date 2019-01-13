d41bf1472d188f30184f80961c8500b32048a401
rbozzini <rossellabozzini@gmail.com>
2019-01-13 20:28:54 +0100

    Sistemazione path relativi

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -1,6 +1,7 @@
 from archivecloner.backupper import BackupReport, CopyFile, RULE_ONCE, RULE_NEWER
 from archivecloner.backup_report import BackupReport
 import json
+import os
 
 
 rules = {'nef': RULE_ONCE, 
@@ -13,8 +14,16 @@ rules = {'nef': RULE_ONCE,
 def test_backup_folders_couple():
     report = BackupReport()
     report.start()
-    bkp = CopyFile("/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/source",
-                   "/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/Dest", rules, report)
+
+    dirname = os.path.dirname(__file__)
+    source_root = os.path.join(dirname, 'resources/backup/source')
+    dest_root = os.path.join(dirname, 'resources/backup/dest')
+
+    #bkp = CopyFile("/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/source",
+ #                  "/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/Dest", rules, report)
+
+    bkp = CopyFile(source_root, dest_root, rules, report)
+
     bkp.backup(True)
     report.end()
 
@@ -27,22 +36,22 @@ def test_backup_folders_couple():
     added_files_data = backup_info_data.get("added_files")
     assert isinstance(added_files_data, list)
     assert len(added_files_data) == 2
-    assert added_files_data[0] == "/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/Dest/subfolder-renamed/immagine7.NEF"
-    assert added_files_data[1] == "/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/Dest/subfolder-renamed/immagine6.tiff"
+    assert added_files_data[0].endswith("backup/dest/subfolder-renamed/immagine7.NEF")
+    assert added_files_data[1].endswith("backup/dest/subfolder-renamed/immagine6.tiff")
 
     deleted_files_data = backup_info_data.get("deleted_files")
     assert isinstance(deleted_files_data, list)
     assert len(deleted_files_data) == 2
-    assert deleted_files_data[0] == "/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/Dest/subfolder2/immagine7.NEF"
-    assert deleted_files_data[1] == "/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/Dest/subfolder2/immagine6.tiff"
+    assert deleted_files_data[0].endswith("backup/dest/subfolder2/immagine7.NEF")
+    assert deleted_files_data[1].endswith("backup/dest/subfolder2/immagine6.tiff")
 
     no_ruled_files_data = backup_info_data.get("no_ruled_files")
     assert isinstance(no_ruled_files_data, list)
     assert len(no_ruled_files_data) == 1
-    assert no_ruled_files_data[0] == "/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/source/.DS_Store"
+    assert no_ruled_files_data[0].endswith("resources/backup/source/.DS_Store")
 
     updated_files_data = backup_info_data.get("updated_files")
     assert isinstance(updated_files_data, list)
     assert len(updated_files_data) == 1
-    assert updated_files_data[0] == "/Users/rossellabozzini/Dev/archivecloner/tests/resources/backup/Dest/immagine1.jpeg"
+    assert updated_files_data[0].endswith("resources/backup/dest/immagine1.jpeg")
         
